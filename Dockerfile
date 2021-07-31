4d99ff0298edeb8cf3228acffd2a3f1f3959aed0
JungYeon Kim <frost917@frost917.com>
2021-07-31 23:01:36 +0900

    sync new func

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ FROM python:3.9.5-slim-buster AS python
 
 COPY --from=source /customer-manager /customer-manager
 WORKDIR /customer-manager
+VOLUME [ "/customer-manager" ]
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- src/dbconn.py
+++ src/dbconn.py
@@ -7,6 +7,7 @@ class Database:
         host = os.getenv("DB_HOST")
         database = os.getenv("DB_DATABASE")
         # DB_PORT 환경변수가 없는 경우 기본값 5432 부여
+        port = 0
         port = os.getenv("DB_PORT") if os.getenv("DB_PORT") is not None else port = 5432
 
         # DB USER
@@ -19,6 +20,7 @@ class Database:
         except db.DatabaseError as err:
             print(err)
 
+    # 고객 명단 불러오기
     def getCustomerTuple(self, uuid):
         try:
             self.cur.execute("""
@@ -30,15 +32,69 @@ class Database:
         except db.DatabaseError as err:
             print(err)
 
+    # 고객 UUID 불러오기
     def getCustomerID(self, uuid, name):
         try:
             self.cur.execute("""
                 SELECT customerID 
                 FROM customer
-                WHERE uuid = %s, name = %s""",
+                WHERE uuid = %s AND name = %s""",
                 (uuid, name,))
             return self.cur.fetchone()
         except db.DatabaseError as err:
             print(err)
 
-    def getJobsTuple(self):
+    # 모든 작업 내역 불러오기
+    def getJobsTuple(self, uuid):
+        try:
+            self.cur.execute("""
+                SELECT jobID, jobs
+                FROM jobs_list
+                where uuid = %s""",
+                (uuid,))
+        except db.DatabaseError as err:
+            print(err)
+
+    # 특정 손님의 작업 내역 불러오기
+    def getJobsSpecipic(self, uuid, customerID):
+        try:
+            self.cur.execute("""
+                SELECT jobID, jobs
+                FROM jobs_list
+                where uuid = %s AND customerID = %s""",
+                (uuid, customerID,))
+        except db.DatabaseError as err:
+            print(err)
+    
+    # 작업 기록 불러오기
+    def getJobHistory(self, jobID):
+        try:
+            self.cur.execute("""
+                SELECT jobHistory
+                FROM misc
+                where jobID = %s""",
+                (jobID,))
+        except db.DatabaseError as err:
+            print(err)
+
+    # 모든 예약 불러오기
+    def getReserveTuple(self, uuid):
+        try:
+            self.cur.execute("""
+                SELECT customerID, reservedTime
+                FROM reserve
+                where uuid = %s""",
+                (uuid,))
+        except db.DatabaseError as err:
+            print(err)
+
+    # 특정 손님의 예약 불러오기
+    def getReserveSpecipic(self, customerID, uuid):
+        try:
+            self.cur.execute("""
+                SELECT customerID, reservedTime
+                FROM reserve
+                where uuid = %s""",
+                (uuid,))
+        except db.DatabaseError as err:
+            print(err)
