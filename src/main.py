4932582c6931b11845e0a96dd179fc6906e9c068
JungYeon Kim <frost917@frost917.com>
2021-08-22 03:07:29 +0900

    깨진 함수 수정 #2

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, redirect, Response
+from flask import Flask, request, g, jsonify, redirect, Response
 from flask.helpers import make_response
 from flask.templating import render_template
 import os
+from auth.flaskAuthVerify import tokenVerify
 
 app = Flask(__name__)
 app.secret_key = os.urandom(20)
@@ -16,20 +17,9 @@ app.config['SERVER_NAME'] = "0.0.0.0:5000"
 
 # 굳이 필요는 없지만 그냥 한번 만들어봄
 @app.route("/", method=['GET', 'POST'])
+@tokenVerify
 def index():
-    try:
-        token = request.headers.get('Authorization')
-    except:
-        from msg.jsonMsg import dataMissingJson
-        return Response(dataMissingJson(), status=400, mimetype="application/json")
-
-    # JWT Decode 결과가 None인 경우
-    # 토큰이 잘못된 것으로 간주
-    from auth.jwtTokenProcess import decodeToken
-    userID = decodeToken(token=token, retResource="userID")
-    if userID is None:
-        from msg.jsonMsg import tokenInvalid
-        return Response(tokenInvalid(), status=401, mimetype="application/json")
+    userID = g.get("userID")
 
     convDict = dict()
     convDict['userID'] = userID
