37db47c1f85c9270114eb4a1f06d235b6f570db4
frost917 <syjk506@gmail.com>
2021-09-04 14:49:21 +0000

    blueprint 추가

--- src/main.py
+++ src/main.py
@@ -2,28 +2,49 @@ import os
 
 from flask import Flask, Response, g
 
-import auth.flaskAuthProcess
-import auth.flaskAuthRefresh
-import customers.flaskAddNewCustomer
-import customers.flaskDeleteCustomer
-import customers.flaskGetCustomerData
-import customers.flaskGetCustomerList
-import customers.flaskUpdateCustomerInfo
 from auth.flaskAuthVerify import tokenVerify
 
 app = Flask(__name__)
 app.secret_key = os.urandom(20)
 
 # auth
+import auth.flaskAuthProcess
+import auth.flaskAuthRefresh
+
 app.register_blueprint(auth.flaskAuthProcess.manager)
 app.register_blueprint(auth.flaskAuthRefresh.manager)
 
 # customers
+import customers.flaskAddNewCustomer
+import customers.flaskDeleteCustomer
+import customers.flaskGetCustomerData
+import customers.flaskGetAllCustomer
+import customers.flaskUpdateCustomerInfo
+
 app.register_blueprint(customers.flaskAddNewCustomer.manager)
 app.register_blueprint(customers.flaskDeleteCustomer.manager)
 app.register_blueprint(customers.flaskGetCustomerData.manager)
-app.register_blueprint(customers.flaskGetCustomerList.manager)
+app.register_blueprint(customers.flaskGetAllCustomer.manager)
 app.register_blueprint(customers.flaskUpdateCustomerInfo.manager)
+
+# jobs
+import jobs.flaskAddJobHistory
+import jobs.flaskGetAllJobHistory
+import jobs.flaskGetJobHistory
+import jobs.flaskGetSpecJobHistory
+
+app.register_blueprint(jobs.flaskAddJobHistory.manager)
+app.register_blueprint(jobs.flaskGetAllJobHistory.manager)
+app.register_blueprint(jobs.flaskGetJobHistory.manager)
+app.register_blueprint(jobs.flaskGetSpecJobHistory.manager)
+
+# visit-history
+import visitHistory.flaskGetAllVisitHistory
+import visitHistory.flaskGetVisitHistory
+
+app.register_blueprint(visitHistory.flaskGetAllVisitHistory.manager)
+app.register_blueprint(visitHistory.flaskGetVisitHistory.manager)
+
 
 # TODO jsonify는 다 json.dumps로 교체할 것
 
@@ -47,9 +68,7 @@ def index():
     from json import dumps
     helloUser = dumps(convDict)
     return Response(helloUser, status=200, mimetype="application/json")
-
-# @app.route('/visit-history')
-
+    
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=5000)
