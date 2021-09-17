48ab71a17839687565f01f4ca103f3aa153d3f99
frost917 <syjk506@gmail.com>
2021-09-17 07:41:20 +0000

    로그인 페이지 블루프린트 연동

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -5,6 +5,14 @@ from flask.templating import render_template
 app = Flask(__name__)
 app.secret_key = os.urandom(20)
 
+# 로그인 페이지 블루프린트
+
+from login import login
+from login import loginPage
+
+app.register_blueprint(login.front)
+app.register_blueprint(loginPage.front)
+
 @app.route('/')
 def index():
     return render_template('index.html')
