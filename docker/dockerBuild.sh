c90d3cd8bcfbc58c14d1f4621ae82c9545a55272
frost917 <syjk506@gmail.com>
2021-09-23 02:14:35 +0900

    개발 플랫폼 변경

--- docker/dockerBuild.sh
+++ docker/dockerBuild.sh
@@ -8,6 +8,6 @@ cd  ${gitDir}/
 docker buildx build \
     --push \
     --no-cache \
-    --platform=linux/arm64/v8,linux/amd64 \
+    --platform=linux/amd64 \
     -t ${REGISTRY}/customer-manager:dev \
     ./
