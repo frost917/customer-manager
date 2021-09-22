7d926954871b9e7c28840b570cf3fbba28d1575d
frost917 <syjk506@gmail.com>
2021-09-23 02:13:05 +0900

    개발용 도커파일 추가

---
+++ docker/dockerBuild.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash 
+gitDir=$(dirname $(realpath $0))
+ver="dev"
+
+#  build
+cd  ${gitDir}/
+
+docker buildx build \
+    --push \
+    --no-cache \
+    --platform=linux/arm64/v8,linux/amd64 \
+    -t ${REGISTRY}/customer-manager:dev \
+    ./
