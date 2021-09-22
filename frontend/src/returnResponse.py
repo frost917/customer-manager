9fbec0c43ea559fcf1e406b57587fce0990e1c27
frost917 <syjk506@gmail.com>
2021-09-22 16:16:24 +0000

    변수 이름 변경

--- frontend/src/returnResponse.py
+++ frontend/src/returnResponse.py
@@ -6,12 +6,12 @@ from flask import g, make_response
 def makeResponse(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        render_template = func(*args, **kwargs)
+        template = func(*args, **kwargs)
 
         accessToken = g.get('accessToken')
         refreshToken = g.get('refreshToken')
 
-        result = make_response(render_template)
+        result = make_response(template)
         result.set_cookie('accessToken', accessToken, max_age=timedelta(hours=3), httponly=True)
         result.set_cookie('refreshToken', refreshToken, max_age=timedelta(hours=4320), httponly=True)
         return result
