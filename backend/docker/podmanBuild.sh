0f4bd56986160e004e03f128309884639abc2c5d
frost917 <syjk506@gmail.com>
2021-09-25 08:48:12 +0000

    podman build용 스크립트 추가

---
+++ backend/docker/podmanBuild.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/sh
+
+gitDir=$(dirname $(realpath $0))
+serviceName="customer-manager"
+
+cd  ${gitDir}/backend/docker/Dockerfile
+podman build \
+    --pull \
+    --no-cache \
+    --platform=linux/arm64/v8,linux/amd64 \
+    -t ${REGISTRY}/$serviceName:backend \
+    ./
+
+podman image push \
+    ${REGISTRY}/$serviceName:backend
---
+++ docker/podmanBuild.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/sh
+
+gitDir=$(dirname $(realpath $0))
+serviceName="customer-manager"
+
+cd ${gitDir}/frontend/docker/Dockerfile
+podman build \
+    --pull \
+    --no-cache \
+    --platform=linux/arm64/v8,linux/amd64 \
+    -t ${REGISTRY}/$serviceName:frontend
+    ./
+
+podman image push \
+    ${REGISTRY}/$serviceName:frontend
